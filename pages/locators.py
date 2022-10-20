170f94c1b2dd9d82f3f560163236a86d3b1fe14a
lomicster <lomicster@gmail.com>
2022-10-19 23:14:48 -0700

    Added locators

---
+++ pages/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+
+    CATALOGUE_LINK = (By.XPATH, "//ul[@id='browse']//ul//a")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,9 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+
 
 class MainPage(BasePage):
 
     def go_to_catalogue(self):
-        self.browser.find_element(By.XPATH, "//ul[@id='browse']//ul//a").click()
+        self.browser.find_element(*MainPageLocators.CATALOGUE_LINK).click()
