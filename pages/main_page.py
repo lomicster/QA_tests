7631b723d5868d7ad8519460e536df70c07f804c
lomicster <lomicster@gmail.com>
2022-10-19 23:05:25 -0700

    Test with import modules

---
+++ pages/main_page.py
@@ -0,0 +1,7 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage):
+
+    def go_to_catalogue(self):
+        self.browser.find_element(By.XPATH, "//ul[@id='browse']//ul//a").click()
--- test_main_page.py
+++ test_main_page.py
@@ -1,28 +1,24 @@
 import time
+from pages.main_page import MainPage
 import pytest
-from selenium.webdriver.common.by import By
 
 
 link = 'http://selenium1py.pythonanywhere.com/ru'
 
 
-class TestMainPage():
+# class TestMainPage():
+#     @pytest.mark.open_page
+def test_guest_can_go_to_catalogue(browser):
+    page = MainPage(browser, link)
+    page.open_page()
+    page.go_to_catalogue()
 
-    @pytest.mark.open_page
-    def test1(self, browser):
-        #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        browser.get(link)
+    # @pytest.mark.view_products
+    # def test2(browser):
+    #     browser.get(link)
+    #     time.sleep(5)
 
-    @pytest.mark.view_products
-    def test2(self, browser):
-        #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        browser.get(link)
-        browser .find_element(By.XPATH, "//ul[@id='browse']//ul//a").click()
-        time.sleep(5)
+    # @pytest.mark.xfail
+    # def test3(browser):
+    #     browser.get(link)
 
-
-    @pytest.mark.xfail
-    def test2(self, browser):
-        # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        browser.get(link)
-        browser.find_element(By.XPATH, "//ul[@id='browse']//ul//a").click()
